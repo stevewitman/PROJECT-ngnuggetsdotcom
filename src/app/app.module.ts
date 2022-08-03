51d8dc0f6513264eae512fa8820f5992402f47db
Steve Witman <stevewitman@gmail.com>
2022-08-03 12:12:03 -0600

    add @angular/material and @angular/fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +16,17 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ngnuggetsdotcom-firebase',
+    appId: '1:800241353749:web:a65417880184b792977a1c',
+    storageBucket: 'ngnuggetsdotcom-firebase.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBhAu4JyzZQFkoG1bRyj6QmID_rfnZjpQk',
+    authDomain: 'ngnuggetsdotcom-firebase.firebaseapp.com',
+    messagingSenderId: '800241353749',
+    measurementId: 'G-L97Z1NBNQJ',
+  },
   production: false
 };
 
